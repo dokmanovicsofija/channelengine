dcead018e3e69580a84abb87c42a4c38e5d45b2c
Sofija Dokmanovic <sofija.dokmanovic@logeecom.com>
2024-09-10 13:23:27 +0200

    Add comments for sync.js
    ISSUE: MIDU-981

--- views/js/sync.js
+++ views/js/sync.js
@@ -1,9 +1,11 @@
+// Add an event listener to the 'syncButton' that triggers when it's clicked
 document.getElementById('syncButton').addEventListener('click', function () {
     document.getElementById('syncStatusText').textContent = 'In progress...';
     document.getElementById('syncStatusText').className = 'status-progress';
 
     const syncUrl = admin_sync_link;
 
+    // Use the Fetch API to send a POST request to the sync URL
     fetch(syncUrl, {
         method: 'POST',
         headers: {
@@ -21,6 +23,7 @@ document.getElementById('syncButton').addEventListener('click', function () {
                 document.getElementById('syncStatusText').className = 'status-error';
             }
         })
+        // Catch any errors that occur during the fetch request
         .catch(error => {
             document.getElementById('syncStatusText').textContent = 'Error';
             document.getElementById('syncStatusText').className = 'status-error';
